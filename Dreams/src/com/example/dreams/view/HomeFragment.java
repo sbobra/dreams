38a9fa565ca4f30e0329f714d4c08444db807e55
Michael Kwan <kwogger@gmail.com>
2013-11-10 22:07:36 -0500

    Removed unused import

--- Dreams/src/com/example/dreams/view/HomeFragment.java
+++ Dreams/src/com/example/dreams/view/HomeFragment.java
@@ -12,7 +12,6 @@ import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import butterknife.InjectView;
 import butterknife.OnClick;
